fc414419a748820f2153992cf06921002dc8d079
Samiul Hasan <samiulxhasan650@gmail.com>
2022-05-04 02:10:45 +0600

    database car detials update api done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -18,28 +18,58 @@ async function run() {
         const carCollections = client.db('XtremeCar').collection('cars')
 
         // all cars api
-        app.get('/cars', async(req, res)=>{
+        //http://localhost:5000/cars
+        app.get('/cars', async (req, res) => {
             const query = {};
             const cursor = carCollections.find(query);
             const cars = await cursor.toArray();
             res.send(cars)
         })
 
+        // Post API
         // Data format
         // {
         //     "name":"Honda Civic 2022",
-        //     "images":"https://raw.githubusercontent.com/SamiulxHasanx07/xtreme-cars-images/main/cars/2022-honda-civic.jpg",
+        //     "img":"https://raw.githubusercontent.com/SamiulxHasanx07/xtreme-cars-images/main/cars/2022-honda-civic.jpg",
         //     "price":"35000",
         //     "qty":"80",
         //     "supplie": "Honda",
+        //     "brand": "Honda"
         //     "des":"Best wolrd wide car brand and car provider"
         //     }
-        app.post('/cars', async(req, res)=>{
+        app.post('/cars', async (req, res) => {
             const data = req.body;
             const result = await carCollections.insertOne(data);
             res.send(result)
         })
 
+        // data format
+        // {
+        // "name":"Samiul Hasan",
+        // "img":"https://raw.githubusercontent.com/SamiulxHasanx07/xtreme-cars-images/main/cars/2022-honda-civic.jpg",
+        // "price":"888888",
+        // "qty":"01",
+        // "supplier": "Audi",
+        // "brand":"Audi",
+        // "des":"Best wolrd wide car brand and car provider"
+        // }
+        // Update Car Details API
+        app.put('/car/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const car = req.body;
+            const { name, img, price, qty, supplier, brand, des } = car;
+            console.log(car);
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    name: name, img: img, price: price, qty: qty, supplier: supplier, brand: brand, des: des
+                },
+            };
+            const result = await carCollections.updateOne(filter, updateDoc, options);
+            res.send(result)
+        })
     }
     finally {
 
