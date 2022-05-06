0abee79f2f7b76f54b88b7fa35d45aa8c8ffd6f0
Samiul Hasan <samiulxhasan650@gmail.com>
2022-05-07 04:28:56 +0600

    JWT Implementation done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
@@ -8,6 +9,25 @@ const app = express();
 app.use(cors())
 app.use(express.json())
 
+// verify user token
+function verifyJWT(req, res, next) {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ message: 'You Cant Access Data' })
+    }
+
+    const token = authorization.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.statue(403).send({ message: 'You Cant Access Data, Forbidden authorization' })
+        }
+        // console.log('decoded', decoded);
+        req.decoded = decoded;
+
+    })
+    // console.log(authorization);
+    next();
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vvsc7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -26,22 +46,36 @@ async function run() {
             res.send(cars)
         })
 
+        // User Login
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '2d'
+            })
+            res.send({ accessToken })
+        })
         //single car api
         //http://localhost:5000/car/id
         app.get('/car/:id', async (req, res) => {
             const id = req.params.id;
-            const query = { _id: ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const result = await carCollections.findOne(query);
             res.send(result);
         })
 
         // my item api
-        app.get('/myitems/:email', async(req, res)=>{
+        app.get('/myitems/:email', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
             const searchData = req.params.email;
-            const query = {email: searchData};
-            const cursor = carCollections.find(query);
-            const result = await cursor.toArray();
-            res.send(result);
+            if (decodedEmail === searchData) {
+                const query = { email: searchData };
+                const cursor = carCollections.find(query);
+                const result = await cursor.toArray();
+                res.send(result);
+            }else{
+                res.status(403).send({message:'forbidden access you cant access this data'})
+            }
+
         })
 
         // Post API
@@ -92,14 +126,14 @@ async function run() {
         // {
         //     "newQty":"50"
         // }
-        app.put('/carqty/:id', async (req, res)=>{
+        app.put('/carqty/:id', async (req, res) => {
             const id = req.params.id;
             const data = req.body;
-            const {newQty} =  data;
-            const filter = {_id:ObjectId(id)};
-            const options = {upsert:true}
+            const { newQty } = data;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true }
             const updateDoc = {
-                $set:{
+                $set: {
                     qty: newQty
                 }
             }
@@ -108,13 +142,13 @@ async function run() {
         })
 
         // Delete Car
-        app.delete('/car', async(req, res)=>{
+        app.delete('/car', async (req, res) => {
             const id = req.query._id;
-            const filter =  {_id:ObjectId(id)}
-            const result =  await carCollections.deleteOne(filter);
+            const filter = { _id: ObjectId(id) }
+            const result = await carCollections.deleteOne(filter);
             res.send(result)
             console.log(id);
-            
+
         })
     }
     finally {
